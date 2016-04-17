d20c3440a1ea264aa89cb2345f3a9445c93234ec
ivanybma <ivanybma@ivanybmas-MacBook-Pro.local>
2016-04-16 17:07:05 -0700

    update dao

--- src/main/java/com/houserental/dao/Dao.java
+++
@@ -1,9 +0,0 @@
-package com.houserental.dao;
-
-/**
- * Created by ivanybma on 4/16/16.
- */
-public class Dao {
-    private int a;
-    private int b;
-}
---
+++ src/main/java/dao/dao.java
@@ -0,0 +1,7 @@
+package dao;
+
+/**
+ * Created by ivanybma on 4/16/16.
+ */
+public interface dao {
+}
